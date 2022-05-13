c5fcdd6e5b2a7dd76a6a5bc9fede4bb671fa5d7c
GitHubEason <defend.sen@gmail.com>
2022-05-13 17:17:07 +1000

    feat: cc-0010 add Swagger

--- app.ts
+++ app.ts
@@ -1,18 +1,23 @@
 import { port } from "./src/config/config";
+
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+
 import { logger } from "./src/utils/logger";
+import { swaggerDoc } from "./src/utils/swagger";
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("tiny"));
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 /**
  * Server Activation
  */
+
 app.listen(port, () => {
   logger.info(`Listening on port ${port}`);
 });
