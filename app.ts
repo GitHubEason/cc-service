54b7f273bd569ad8c358db883c2ab002412d1e5f
GitHubEason <defend.sen@gmail.com>
2022-05-14 20:51:08 +1000

    chore: cc-0010 add cors && change yarn to npm

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 
 import { logger } from "./src/utils/logger";
 import { swaggerDoc } from "./src/utils/swagger";
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 app.use(morgan("tiny"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 /**
--- src/middleware/cors.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const cors = (req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  next();
-};
